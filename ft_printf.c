992b3700cc0fee1a3c59a4606938e85a6416d556
Guilherme Braga gomes <gbraga-g@cbr7s1.42barcelona.com>
2022-05-26 16:25:23 +0200

    Started printf with one char

--- ft_printf.c
+++ ft_printf.c
@@ -6,23 +6,26 @@
 /*   By: gbraga-g <gbraga-g@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 15:27:54 by gbraga-g          #+#    #+#             */
-/*   Updated: 2022/05/26 15:44:28 by gbraga-g         ###   ########.fr       */
+/*   Updated: 2022/05/26 16:24:53 by gbraga-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdarg.h>
+
 int ft_printf(const char *, ...)
 {
-//Taught by other student:
-char *pepito;
-va_list element;
+	va_list arg;
+	printf("%s\n", va_arg(arg, char *));
+	printf("%s\n", va_arg(arg, char *));
+	va_end(arg);
+	return (0);
+}
 
-
-Pepito = “que tal”;
-printf(“hola %s %s %d\n”, pepito, “buenos dias”, 150);
-
-
-ft_putstr(va_arg(element,char *));
-ft_putstr(va_arg(element, char *))
-va_end(element);
-Return (blabla);
+int main(void)
+{
+	char c = 'A';
+	char d = 'B';
+	ft_printf("%c %c", c, d);
+	
 }
