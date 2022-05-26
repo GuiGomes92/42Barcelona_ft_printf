04a7ee2d590e41619fd7b173fe0077ee8671ff2f
Guilherme Braga gomes <gbraga-g@cbr7s1.42barcelona.com>
2022-05-26 15:45:38 +0200

    Created Makefile and main function file

---
+++ ft_printf.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbraga-g <gbraga-g@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/26 15:27:54 by gbraga-g          #+#    #+#             */
+/*   Updated: 2022/05/26 15:44:28 by gbraga-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_printf(const char *, ...)
+{
+//Taught by other student:
+char *pepito;
+va_list element;
+
+
+Pepito = “que tal”;
+printf(“hola %s %s %d\n”, pepito, “buenos dias”, 150);
+
+
+ft_putstr(va_arg(element,char *));
+ft_putstr(va_arg(element, char *))
+va_end(element);
+Return (blabla);
+}
