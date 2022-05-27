c312e341351c0e47d1d292f275c79f9079d7f392
Guilherme Braga gomes <gbraga-g@cbr7s6.42barcelona.com>
2022-05-27 17:00:09 +0200

    Changes to Makefile, char and % sign implemented

--- ft_printf.c
+++ ft_printf.c
@@ -6,27 +6,55 @@
 /*   By: gbraga-g <gbraga-g@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 15:27:54 by gbraga-g          #+#    #+#             */
-/*   Updated: 2022/05/26 18:07:06 by gbraga-g         ###   ########.fr       */
+/*   Updated: 2022/05/27 16:47:08 by gbraga-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
+
+static void ft_putchar(int c)
+{
+	write(1, &c, 1);
+}
+
+static int ft_check_type(va_list arg, char type)
+{
+	if (type == 'c')
+		ft_putchar(va_arg(arg, int));
+	else if (type == '%')
+		ft_putchar('%');
+	return (0);
+}
 
 int ft_printf(const char *str, ...)
 {
 	va_list arg;
+	int		i;
+	
+	i = 0;
 	va_start(arg, str);
-	printf("%s\n", va_arg(arg, char *));
-	printf("%s\n", va_arg(arg, char *));
+	while (str[i] != '\0')
+	{
+		if(str[i] == '%')
+		{
+			ft_check_type(arg, str[i + 1]);
+			i++;
+		}
+		else
+			ft_putchar(str[i]);
+		i++;
+	}
 	va_end(arg);
 	return (0);
 }
 
 int main(void)
 {
-	char c = 'A';
-	char d = 'B';
-	ft_printf("%c %c", c, d);
-	
+	char c = 'B';
+	ft_printf("Hello %c", c);
+	printf("\n");
+	ft_printf("Hello %%", c);
+	printf("\n");
 }
