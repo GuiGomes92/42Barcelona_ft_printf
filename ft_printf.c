b3cbdad81bf9afc9fd9a55486c2d721eda63073d
Guilherme Braga gomes <gbraga-g@cbr7s6.42barcelona.com>
2022-05-27 17:05:14 +0200

    Implemented str print

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: gbraga-g <gbraga-g@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 15:27:54 by gbraga-g          #+#    #+#             */
-/*   Updated: 2022/05/27 16:47:08 by gbraga-g         ###   ########.fr       */
+/*   Updated: 2022/05/27 17:04:56 by gbraga-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,26 @@ static void ft_putchar(int c)
 	write(1, &c, 1);
 }
 
+static void ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
 static int ft_check_type(va_list arg, char type)
 {
 	if (type == 'c')
 		ft_putchar(va_arg(arg, int));
 	else if (type == '%')
 		ft_putchar('%');
+	else if (type == 's')
+		ft_putstr(va_arg(arg, char *));
 	return (0);
 }
 
@@ -53,8 +67,19 @@ int ft_printf(const char *str, ...)
 int main(void)
 {
 	char c = 'B';
+	char *str = "Que tal?";
 	ft_printf("Hello %c", c);
 	printf("\n");
 	ft_printf("Hello %%", c);
 	printf("\n");
+	ft_printf("Hello %s", str);
+
 }
+/*
+• %p The void * pointer argument has to be printed in hexadecimal format.
+• %d Prints a decimal (base 10) number.
+• %i Prints an integer in base 10.
+• %u Prints an unsigned decimal (base 10) number.
+• %x Prints a number in hexadecimal (base 16) lowercase format.
+• %X Prints a number in hexadecimal (base 16) uppercase format.
+*/
