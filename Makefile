46de6b7b1f2c028ab9c914622b5c61c4a1436af8
Guilherme Braga gomes <gbraga-g@cbr8s4.42barcelona.com>
2022-05-28 17:16:47 +0200

    Added integer rule and changed Makefile

--- Makefile
+++ Makefile
@@ -6,18 +6,22 @@
 #    By: gbraga-g <gbraga-g@student.42barcel>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/05/26 15:17:34 by gbraga-g          #+#    #+#              #
-#    Updated: 2022/05/27 15:40:06 by gbraga-g         ###   ########.fr        #
+#    Updated: 2022/05/28 16:48:29 by gbraga-g         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME	=	libftprintf.a
 
+SRCS	=	ft_printf.c
 CC		=	gcc
 CFLAGS	=	-Wall -Wextra -Werror
 RM      =   rm -f
+OBJS	=	$(SRCS:%.c=%.o)
+%.o: %.c
+		${CC} ${CFLAGS} -I./ -c $< -o $@
 
 all: ${NAME}
-${NAME}:${OBJS} libftprint.h
+${NAME}:${OBJS} ft_printf.h
 	ar -crs ${NAME} ${OBJS}
 
 clean:
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: gbraga-g <gbraga-g@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 15:27:54 by gbraga-g          #+#    #+#             */
-/*   Updated: 2022/05/27 17:04:56 by gbraga-g         ###   ########.fr       */
+/*   Updated: 2022/05/28 17:15:25 by gbraga-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,33 @@ static void ft_putstr(char *str)
 	}
 }
 
+static int ft_putint(int i)
+{
+	char	c;
+
+	if (i == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+	}
+	else if (i < 0)
+	{
+		write(1, "-", 1);
+		i = -i;
+		ft_putint(i);
+	}
+	else if (i >= 10)
+	{
+		ft_putint(i / 10);
+		ft_putint(i % 10);
+	}
+	else
+	{
+		c = i + '0';
+		write(1, &c, 1);
+	}
+	return (0);
+}
+
 static int ft_check_type(va_list arg, char type)
 {
 	if (type == 'c')
@@ -39,6 +66,8 @@ static int ft_check_type(va_list arg, char type)
 		ft_putchar('%');
 	else if (type == 's')
 		ft_putstr(va_arg(arg, char *));
+	else if (type == 'i')
+		ft_putint(va_arg(arg, int));
 	return (0);
 }
 
@@ -66,19 +95,18 @@ int ft_printf(const char *str, ...)
 
 int main(void)
 {
+	int	i = 23;
 	char c = 'B';
 	char *str = "Que tal?";
-	ft_printf("Hello %c", c);
-	printf("\n");
-	ft_printf("Hello %%", c);
-	printf("\n");
-	ft_printf("Hello %s", str);
+	ft_printf("Hello %c\n", c);
+	ft_printf("Hello %%\n", c);
+	ft_printf("Hello %s\n", str);
+	ft_printf("Hello, %i\n" , i);
 
 }
 /*
 • %p The void * pointer argument has to be printed in hexadecimal format.
 • %d Prints a decimal (base 10) number.
-• %i Prints an integer in base 10.
 • %u Prints an unsigned decimal (base 10) number.
 • %x Prints a number in hexadecimal (base 16) lowercase format.
 • %X Prints a number in hexadecimal (base 16) uppercase format.
